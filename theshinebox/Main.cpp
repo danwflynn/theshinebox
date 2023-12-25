bda0389a3db12fc95aa26cdd4bbb5e745085ea18
Daniel Flynn <flynn.da@northeastern.edu>
2023-12-24 23:21:33 -0500

    Add project files.

---
+++ theshinebox/Main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        // Draw your SFML graphics here
+        window.display();
+    }
+
+    return 0;
+}
