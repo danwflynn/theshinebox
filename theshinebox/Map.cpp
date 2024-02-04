7b692c0c9325ff0928fc46e7b212da0c2dd5e458
Daniel Flynn <flynn.da@northeastern.edu>
2024-02-04 14:24:29 -0500

    Some extra platforms

--- theshinebox/Map.cpp
+++ theshinebox/Map.cpp
@@ -8,7 +8,11 @@ Map::Map()
 	this->baseGround = bg;
 
 	Platform plat1 = Platform(600, 800, 770);
+	Platform plat2 = Platform(400, 600, 620);
+	Platform plat3 = Platform(800, 1000, 620);
 	this->platforms.push_back(plat1);
+	this->platforms.push_back(plat2);
+	this->platforms.push_back(plat3);
 }
 
 bool Map::touchingGround(float xLeft, float xRight, float yBottom, float verticalSpeed)
